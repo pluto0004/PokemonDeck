30d71eacf339526b36e2e31c6db5df9594c51884
mihoogura <miho@ryu.games>
2021-11-09 17:18:52 +0900

    Add AppProvider

--- app/src/main/java/com/example/pokedecks/AppProvider.kt
+++ app/src/main/java/com/example/pokedecks/AppProvider.kt
@@ -1,4 +1,64 @@
 package com.example.pokedecks
 
-class AppProvider {
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.content.UriMatcher
+import android.database.Cursor
+import android.net.Uri
+import android.util.Log
+
+/** Provider for the PokeDeck app. This is the only class that knows about [AppDatabase].
+ */
+
+private const val TAG = "AppProvider"
+const val CONTENT_AUTHORITY = "com.example.pokedecks.provider"
+
+private const val TRAINERS = 100
+private const val TRAINERS_ID = 101
+
+val CONTENT_AUTHORITY_URI: Uri = Uri.parse("content://$CONTENT_AUTHORITY")
+
+class AppProvider : ContentProvider() {
+
+    private val uriMatcher by lazy { buildUriMatcher() }
+
+    private fun buildUriMatcher(): UriMatcher {
+        Log.d(TAG, "buildUriMatcher: starts")
+        val matcher = UriMatcher(UriMatcher.NO_MATCH)
+        matcher.addURI(CONTENT_AUTHORITY, TrainerContract.TABLE_NAME, TRAINERS)
+        matcher.addURI(CONTENT_AUTHORITY, "${TrainerContract.TABLE_NAME}/#", TRAINERS_ID)
+
+        return matcher
+    }
+
+    override fun onCreate(): Boolean {
+        Log.d(TAG, "onCreate: starts")
+        return true
+    }
+
+    override fun query(
+        p0: Uri,
+        p1: Array<out String>?,
+        p2: String?,
+        p3: Array<out String>?,
+        p4: String?
+    ): Cursor? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getType(p0: Uri): String? {
+        TODO("Not yet implemented")
+    }
+
+    override fun insert(p0: Uri, p1: ContentValues?): Uri? {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(p0: Uri, p1: String?, p2: Array<out String>?): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(p0: Uri, p1: ContentValues?, p2: String?, p3: Array<out String>?): Int {
+        TODO("Not yet implemented")
+    }
 }
